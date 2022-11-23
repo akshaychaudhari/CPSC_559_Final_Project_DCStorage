6bb0360d7fab0ce9f985dd8400891090ce3767d9
csuftitan <csuftitan@LTMacAiM1165156.lan>
2022-11-23 02:09:41 -0800

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 import Identicon from 'identicon.js';
-import box from '../box.png'
+import cryptoBox from "../assets/logos/tLogo.png"
+import "../../node_modules/font-awesome/css/font-awesome.min.css"
 
 class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark p-0 text-monospace">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={box} width="30" height="30" className="align-top" alt="" />
-          D$t0r@g3
-        </a>
+      <nav className="navbar navbar-light bg-light p-0 text-monospace shadow bg-body rounded">
+          <img src={cryptoBox} width="150" height="70" className="align-top ml-3 pt-1 pb-2" alt="CryptoBox App Logo" />
+          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-home' style={{fontSize: "20px"}}></span> Home </a>
+          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-users' style={{fontSize: "17px"}}></span> Organization</a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search File:" aria-label="Search" />
+            <button style={{backgroundColor: "#5c13ec", color: "white"}} class="btn" type="submit">Search</button>
+          </form>
         <ul className="navbar-nav px-3">
           <li>
             <small id="account">
               <a target="_blank"
                  alt=""
-                 className="text-white"
+                 style={{color: "black"}}
                  rel="noopener noreferrer"
                  href={"https://etherscan.io/address/" + this.props.account}>
                 {this.props.account ? this.props.account.substring(0,6) : "0x0"}...{this.props.account ? this.props.account.substring(38,42) : "0x0"}
