65cf687eed27e4a63056c2e76eb5fd92170f3302
csuftitan <csuftitan@LTMacAiM1165156.lan>
2022-12-03 02:26:51 -0800

    navbar finalised

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,8 @@ class Navbar extends Component {
     return (
       <nav style={{fontFamily: "Cursive"}} className="navbar navbar-light bg-light p-0 shadow bg-body rounded">
           <img src={cryptoBox} width="150" height="70" className="align-top ml-3 pt-1 pb-2" alt="CryptoBox App Logo" />
-          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-home' style={{fontSize: "20px"}}></span> Home </a>
+          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-globe' style={{fontSize: "20px"}}></span> Global </a>
+          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-share' style={{fontSize: "20px"}}></span> Direct </a>
           <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-users' style={{fontSize: "17px"}}></span> Organization</a>
           <form class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search File:" aria-label="Search" />
