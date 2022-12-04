206020172575cd8ee6c47ea320e0eae55a357f71
csuftitan <csuftitan@LTMacAiM1165156.lan>
2022-12-03 20:53:52 -0800

    membership functions now working

--- src/contracts/Membership.sol
+++ src/contracts/Membership.sol
@@ -39,12 +39,12 @@ contract Membership is BuildOrganization{
             }
         }
         if(isMember && isMemberConfirm){
-            for(uint i = orgIndex; i < orgIds.length; i++){
+            for(uint i = orgIndex; i < orgIds.length - 1; i++){
                 addressToOrganizations[msg.sender][i] = addressToOrganizations[msg.sender][i+1];
             }
             delete addressToOrganizations[msg.sender][addressToOrganizations[msg.sender].length - 1];
             addressToOrganizations[msg.sender].length--;
-            for(uint i = memberIndex; i < memberAddresses.length; i++){
+            for(uint i = memberIndex; i < memberAddresses.length - 1; i++){
                 organizationToAddresses[id][i] = organizationToAddresses[id][i+1]; 
             }
             delete organizationToAddresses[id][organizationToAddresses[id].length - 1];
