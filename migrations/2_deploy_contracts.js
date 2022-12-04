7e81fa80c92285c7095f9215eb0732076ae86184
csuftitan <csuftitan@LTMacAiM1165156.lan>
2022-12-03 18:00:41 -0800

    updated migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const DCStorage = artifacts.require("DCStorage");
 const BuildOrganization = artifacts.require("BuildOrganization");
+const Membership = artifacts.require("Membership");
 
 module.exports = function(deployer) {
 	deployer.deploy(DCStorage);
 	deployer.deploy(BuildOrganization);
+	deployer.deploy(Membership);
 };
