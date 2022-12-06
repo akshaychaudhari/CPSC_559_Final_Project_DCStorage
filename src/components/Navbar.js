0a09d17194d2e9f9ee2dc6b33843e066447de6eb
csuftitan <csuftitan@LTMacAiM1165156.lan>
2022-12-05 17:01:41 -0800

    minor change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ class Navbar extends Component {
     return (
       <nav style={{fontFamily: "Cursive"}} className="navbar navbar-light bg-light p-0 shadow bg-body rounded">
           <img src={cryptoBox} width="150" height="70" className="align-top ml-3 pt-1 pb-2" alt="CryptoBox App Logo" />
-          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-globe' style={{fontSize: "20px"}}></span> Global </a>
+          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="/"> <span className='fa fa-globe' style={{fontSize: "20px"}}></span> Global </a>
           <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="/direct"> <span className='fa fa-share' style={{fontSize: "20px"}}></span> Direct </a>
-          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="#"> <span className='fa fa-users' style={{fontSize: "17px"}}></span> Organization</a>
+          <a style={{color: "#5c13ec", fontSize: "18px"}} class="nav-link" aria-current="page" href="/organization"> <span className='fa fa-users' style={{fontSize: "17px"}}></span> Organization</a>
           <form class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search File:" aria-label="Search" />
             <button style={{backgroundColor: "#5c13ec", color: "white"}} class="btn" type="submit">Search</button>
