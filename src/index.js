5ebe5ffdcc30c3466ad30d1e032ab41e8cb806cd
csuftitan <csuftitan@LTMacAir-M1-165156.local>
2022-12-08 01:55:00 -0800

    added additional routes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 import Direct from './components/Direct';
 import {Routes, Route} from "react-router-dom";
+import Organization from "./components/Organization";
+import ViewOrganization from "./components/ViewOrganization";
 
 ReactDOM.render(
 <BrowserRouter>
 <Routes>
     <Route exact path="/" element={<App/>}/>
     <Route path="/direct" element={<Direct/>}/>
+    <Route path="/organization" element={<Organization/>}/>
+    <Route path="/vieworganization" element={<ViewOrganization/>}/>
 </Routes>
 </BrowserRouter>
 , document.getElementById('root'));
