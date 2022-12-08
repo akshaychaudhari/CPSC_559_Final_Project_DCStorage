bcdde578a7279ef4e424fa986871596b1b322612
csuftitan <csuftitan@LTMacAir-M1-165156.local>
2022-12-07 23:04:36 -0800

    organization frontend part 1

---
+++ src/components/ViewOrganization.js
@@ -0,0 +1,126 @@
+import OrganizationUploads from '../abis/OrganizationUploads.json'
+import { Web3Storage } from "web3.storage";
+import React, { Component } from 'react';
+import Navbar from './Navbar'
+import Main from './Main'
+import Web3 from 'web3';
+import './App.css';
+
+const client = new Web3Storage({token: ""});
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else{
+      window.alert("Please install Metamask in your browser to use the application!");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account: accounts[0]});
+    const networkId = await web3.eth.net.getId()
+    const networkData = OrganizationUploads.networks[networkId]
+    if(networkData) {
+      const organizationuploads = new web3.eth.Contract(OrganizationUploads.abi, networkData.address)
+      this.setState({ organizationuploads })
+      const filesCount = await organizationuploads.methods.fileCount().call()
+      this.setState({ filesCount })
+      for (var i = filesCount; i >= 1; i--) {
+        const file = await organizationuploads.methods.files(i).call()
+        console.log(file)
+        this.setState({
+          files: [...this.state.files, file]
+        })
+      }
+    } else {
+      window.alert('Error! The OrganizationUploads contract not deployed on the detected network!');
+    }
+    this.setState({loading: false})
+  }
+
+  captureFile = event => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result),
+        name: file.name,
+        type: file.type,
+        size: file.size,
+        rawFile: file
+      })
+      console.log('buffer', this.state.rawFile)
+    }
+  }
+
+  uploadFile = async description => {
+    // console.log("Submitting file to IPFS...")
+    const rootCid = await client.put([this.state.rawFile]);
+    const info = await client.status(rootCid);
+    console.log(info)
+    console.log(rootCid);
+      this.setState({ loading: true })
+      if(this.state.type === ''){
+        this.setState({type: 'none'})
+      }
+      this.state.organizationuploads.methods.uploadFile(rootCid, this.state.size, this.state.type, this.state.name, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({
+         loading: false,
+         type: null,
+         name: null
+       })
+       window.location.reload()
+      }).on('error', (e) =>{
+        window.alert('Error')
+        this.setState({loading: false})
+      })
+    }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      organizationuploads: null,
+      files: [],
+      loading: false,
+      type: null,
+      name: null
+    }
+    this.uploadFile = this.uploadFile.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar account={this.state.account} />
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main
+              files={this.state.files}
+              captureFile={this.captureFile}
+              uploadFile={this.uploadFile}
+            />
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ViewOrganizationMain.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { convertBytes } from './helpers';
+import moment from 'moment'
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div className="container-fluid text-center">
+        <div className="row">
+          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
+            <div className="content">
+              <p>&nbsp;</p>
+              <h1 style={{color: "#5c13ec", fontFamily: "copperplate"}}>CRYPTO BOX GLOBAL</h1>
+              <div className="card mb-3 mx-auto" style={{ maxWidth: '512px', fontFamily: "cursive", backgroundColor: "black" }}>
+                <h2 className='pt-4' style={{color: "#7c42ef"}}><b>Global Share</b></h2>
+                  <form onSubmit={(event) => {
+                    event.preventDefault()
+                    const description = this.fileDescription.value
+                    this.props.uploadFile(description)
+                  }} >
+                      <div className="form-group">
+                        <br></br>
+                          <input
+                            id="fileDescription"
+                            type="text"
+                            ref={(input) => { this.fileDescription = input }}
+                            className="form-control"
+                            placeholder="description..."
+                            required />
+                      </div>
+                    <input type="file" onChange={this.props.captureFile} className="text-white pb-3"/>
+                    <button type="submit" className="btn btn-block text-white" style={{backgroundColor: "#7c42ef"}}><b>Upload!</b></button>
+                  </form>
+              </div>
+              <p>&nbsp;</p>
+              <table className="table-sm table-bordered text-monospace" style={{ width: '1000px', maxHeight: '450px'}}>
+                <thead style={{ 'fontSize': '15px' }}>
+                  <tr style={{backgroundColor: "#5c13ec", color: "white"}}>
+                    <th scope="col" style={{ width: '10px'}}>ID</th>
+                    <th scope="col" style={{ width: '200px'}}>Name</th>
+                    <th scope="col" style={{ width: '230px'}}>Description</th>
+                    <th scope="col" style={{ width: '120px'}}>Type</th>
+                    <th scope="col" style={{ width: '90px'}}>Size</th>
+                    <th scope="col" style={{ width: '90px'}}>Datetime</th>
+                    <th scope="col" style={{ width: '120px'}}>Uploader</th>
+                    <th scope="col" style={{ width: '120px'}}>View File</th>
+                  </tr>
+                </thead>
+                { this.props.files.map((file, key) => {
+                  return(
+                    <thead style={{ 'fontSize': '12px' }} key={key}>
+                      <tr>
+                        <td>{file.fileId}</td>
+                        <td>{file.fileName}</td>
+                        <td>{file.fileDescription}</td>
+                        <td>{file.fileType}</td>
+                        <td>{convertBytes(file.fileSize)}</td>
+                        <td>{moment.unix(file.uploadTime).format('h:mm:ss A M/D/Y')}</td>
+                        <td>
+                          <a
+                            href={"https://etherscan.io/address/" + file.uploader}
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            {file.uploader.substring(0,10)}...
+                          </a>
+                         </td>
+                        <td>
+                          <a
+                            href={"https://w3s.link/ipfs/" + file.fileHash + "/" + file.fileName}
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            {file.fileHash.substring(0,10)}...
+                          </a>
+                        </td>
+                      </tr>
+                    </thead>
+                  )
+                })}
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
